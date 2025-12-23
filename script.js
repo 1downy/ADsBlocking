33611b3cbdc929f8494b69f1fe90aa793a744a87
Rony <137443099+1downy@users.noreply.github.com>
2025-12-23 09:15:37 +0500

    fixed issues and added top-to-up buttom

--- script.js
+++ script.js
@@ -82,3 +82,19 @@ document.addEventListener(
 	},
 	{ passive: true }
 );
+const backToTopButton = document.querySelector("#back-to-top");
+
+window.addEventListener("scroll", () => {
+	if (window.pageYOffset > 300) {
+		backToTopButton.classList.add("show");
+	} else {
+		backToTopButton.classList.remove("show");
+	}
+});
+
+backToTopButton.addEventListener("click", () => {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	});
+});
